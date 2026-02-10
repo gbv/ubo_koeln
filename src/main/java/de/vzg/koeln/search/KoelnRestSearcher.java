6b84af4849d378e69af0b95ddcfd390204271703
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2026-02-10 13:50:43 +0100

    refactor: update HTTP client to Apache HttpClient 5 and improve error handling

--- src/main/java/de/vzg/koeln/search/KoelnRestSearcher.java
+++ src/main/java/de/vzg/koeln/search/KoelnRestSearcher.java
@@ -1,27 +1,23 @@
 package de.vzg.koeln.search;
 
-import com.google.common.base.Charsets;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpHeaders;
+import org.apache.hc.core5.http.HttpStatus;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.jdom2.Element;
 import org.mycore.common.MCRException;
 import org.mycore.common.config.MCRConfiguration2;
 import org.mycore.common.config.MCRConfigurationException;
 
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
@@ -50,52 +46,44 @@ public class KoelnRestSearcher implements PersonSearcher {
     private static final int ROW_COUNT = 20;
 
     public PersonResult get(String id){
-        final HttpGet get = new HttpGet(getRestURI()+"persons/"+id);
+        final HttpGet get = new HttpGet(getRestURI() + "persons/" + id);
         get.setHeader("Content-Type", CONTENT_TYPE);
-        get.setHeader(HttpHeaders.AUTHORIZATION,"Basic "+ getEncodedAuth());
+        get.setHeader(HttpHeaders.AUTHORIZATION, "Basic " + getEncodedAuth());
 
-        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
-            try (CloseableHttpResponse response = client.execute(get)) {
-                if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                    try (final InputStream content = response.getEntity().getContent()) {
-                        try (final InputStreamReader isr = new InputStreamReader(content, Charsets.UTF_8)) {
-                            final Gson gson = getGson();
-                            return gson.fromJson(isr, PersonResult.class);
-                        }
-                    }
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            return client.execute(get, response -> {
+                if (response.getCode() == HttpStatus.SC_OK) {
+                    String content = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+                    return getGson().fromJson(content, PersonResult.class);
                 } else {
                     return null;
                 }
-            }
+            });
         } catch (IOException e) {
-            throw new MCRException("Error while building HTTPClient!");
+            throw new MCRException("Error while building HTTPClient!", e);
         }
     }
 
     public PersonListResult search(String term) {
-        final HttpPost httpPost = new HttpPost(getRestURI()+"personsearch");
+        final HttpPost httpPost = new HttpPost(getRestURI() + "personsearch");
         httpPost.setHeader("accept", ACCEPT);
         httpPost.setHeader("Content-Type", CONTENT_TYPE);
-        httpPost.setHeader(HttpHeaders.AUTHORIZATION,"Basic "+ getEncodedAuth());
+        httpPost.setHeader(HttpHeaders.AUTHORIZATION, "Basic " + getEncodedAuth());
 
         final String requestBody = buildRequestBody(term);
         httpPost.setEntity(new StringEntity(requestBody, ContentType.APPLICATION_JSON));
 
-        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
-            try (CloseableHttpResponse response = client.execute(httpPost)) {
-                if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                    try (final InputStream content = response.getEntity().getContent()) {
-                        try (final InputStreamReader isr = new InputStreamReader(content, Charsets.UTF_8)) {
-                            final Gson gson = getGson();
-                            return gson.fromJson(isr, PersonListResult.class);
-                        }
-                    }
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            return client.execute(httpPost, response -> {
+                if (response.getCode() == HttpStatus.SC_OK) {
+                    String content = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+                    return getGson().fromJson(content, PersonListResult.class);
                 } else {
                     return new PersonListResult();
                 }
-            }
+            });
         } catch (IOException e) {
-            throw new MCRException("Error while building HTTPClient!");
+            throw new MCRException("Error while building HTTPClient!", e);
         }
     }
 
@@ -169,7 +157,7 @@ public class KoelnRestSearcher implements PersonSearcher {
     }
 
     private String getEncodedAuth() {
-        final byte[] encoded = Base64.getEncoder().encode((getUsername() + ":" + getPassword()).getBytes(Charsets.UTF_8));
+        final byte[] encoded = Base64.getEncoder().encode((getUsername() + ":" + getPassword()).getBytes(StandardCharsets.UTF_8));
         return new String(encoded, StandardCharsets.UTF_8);
     }
 
